9efd9665aa4d6f2f566f8e9ad9b8f5ae12274905
Lastghost33 <tnhn33dyg@gmail.com>
2022-06-20 16:44:35 +0300

    chore: create araba model

---
+++ lib/tutorial/models/araba.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_basics/tutorial/models/base_model.dart';
+
+class Araba extends BaseModel {
+
+  @override
+  void selamla(){
+    print('Arabadan selamla');
+  }
+
+  void arabaFonksiyon (){}
+}

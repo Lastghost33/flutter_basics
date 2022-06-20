f175b0bb74b22b7ec65586ada7cd4b65eaeff11e
Lastghost33 <tnhn33dyg@gmail.com>
2022-06-20 16:44:48 +0300

    chore: beta base model created

---
+++ lib/tutorial/models/beta_base_model.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_basics/tutorial/models/ibase_model.dart';
+
+class BetaBaseModel implements IBaseModel {
+  @override
+  String? id;
+
+  @override
+  DateTime? initializeDate;
+
+  @override
+  DateTime? updatedDate;
+
+  @override
+  void selamla() {
+    // TODO: implement selamla
+  }
+  void betaBaseModelFunction(){
+    
+  }
+}
